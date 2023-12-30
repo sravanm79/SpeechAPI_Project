2c6b0376b8830a0d2bb9cbd6272f1530a9d2e816
Soma Sekhar <>
2023-12-30 17:43:28 +0530

    shell=True is set again

--- run.py
+++ run.py
@@ -10,11 +10,11 @@ server2_process = None
 
 def run_server1():
     if platform.system() != 'Linux':
-        raise "This App runs only on Ubuntu>20.4"
+        raise Exception("This App runs only on Ubuntu>20.4")
     environment_name = 'fairseq'
     activate_command = f"conda activate {environment_name}  && python server1/app.py"
 
-    return subprocess.Popen(activate_command, cwd="server1")
+    return subprocess.Popen(activate_command, shell=True, cwd="server1")
 
 
 def run_server2():
@@ -22,7 +22,7 @@ def run_server2():
         raise "This App runs only on Ubuntu>20.4"
     environment_name = 'pynnote'
     activate_command = f"conda activate {environment_name} && python server2/app.py"
-    return subprocess.Popen(activate_command, cwd="server2")
+    return subprocess.Popen(activate_command, shell=True, cwd="server2")
 
 
 def print_server_info():
