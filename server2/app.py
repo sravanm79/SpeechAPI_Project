b6d526c0ad5dbbab2a2566c1ad2ce038204d2f54
Soma Sekhar <>
2024-01-01 02:42:48 +0530

    server2 response changed to file instead of plain text

--- server2/app.py
+++ server2/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, jsonify, Response
+import os.path
+
+from flask import Flask, request, jsonify, send_file
 from pynnote_diarization import diarize_speaker
 
 app = Flask(__name__)
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 
 @app.route('/diarize', methods=['POST'])
@@ -9,12 +12,16 @@ def diarize_file():
     try:
         data = request.json
         wav_path = data.get('wav_path')
-
-        if not wav_path:
-            raise ValueError("WAV path not provided")
+        print(wav_path)
+        if not os.path.exists(wav_path):
+            raise ValueError("WAV path not exists")
 
         rttm_data = diarize_speaker(wav_path)
-        return Response(rttm_data, mimetype='text/plain')
+        rttm_file_path = f"{PROJECT_ROOT}/uploads/rttm_files/{os.path.basename(wav_path)[:-4]}.rttm"
+        with open(rttm_file_path, 'w') as file:
+            rttm_data.write_rttm(file)
+        return send_file(rttm_file_path)
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
